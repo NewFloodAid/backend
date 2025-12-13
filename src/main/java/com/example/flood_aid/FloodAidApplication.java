22cd1eb40388cf377ab8c39685b3045cc303a4da
ChayathonPP <chayathon_p@cmu.ac.th>
2025-12-13 12:41:57 +0700

    refactor:  fix build config

--- src/main/java/com/example/flood_aid/FloodAidApplication.java
+++ src/main/java/com/example/flood_aid/FloodAidApplication.java
@@ -1,14 +1,20 @@
 package com.example.flood_aid;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 public class FloodAidApplication {
+
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+
 		SpringApplication app = new SpringApplication(FloodAidApplication.class);
 		app.setAdditionalProfiles("dev");
 		app.run(args);
 	}
+
 }
